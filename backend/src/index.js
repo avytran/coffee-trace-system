02f2804299de4a26aaea7afc64a6aa3f1f41bf91
kemdaubosua <thiennhi2k4@gmail.com>
2026-05-25 22:04:34 +0700

    feat(TV2): add cooperative processing flow

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import 'dotenv/config';
 
 import './server.js';
+import './indexer/coopIndexer.js';
 
 console.log('[Entry Point]: src/index.js triggered Server!');
