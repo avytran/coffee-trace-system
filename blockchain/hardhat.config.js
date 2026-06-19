3745c38c5c6afbdf65ab0bbb78ed25b75c7900f6
avytran <tr.anhvy.work@gmail.com>
2026-06-19 22:21:17 +0700

    fix: enable dynamic env for running blockchain

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 const config = {
@@ -11,8 +12,16 @@ const config = {
       },
     },
   },
-  paths: {
-    artifacts: "../backend/artifacts", 
+
+  networks: {
+    localhost: {
+      url: process.env.LOCAL_RPC_URL || "http://127.0.0.1:8545",
+      accounts: process.env.LOCAL_PRIVATE_KEY ? [process.env.LOCAL_PRIVATE_KEY] : []
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: process.env.SEPOLIA_PRIVATE_KEY ? [process.env.SEPOLIA_PRIVATE_KEY] : []
+    }
   }
 };
 
